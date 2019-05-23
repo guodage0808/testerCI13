a05708bdbac557cee9cc23187b2c7d8af821cbed
xiaopeng.guo <2171029637@qq.com>
2019-05-23 22:19:56 +0800

    tester13 news code

--- test/tester.py
+++ test/tester.py
@@ -7,7 +7,8 @@ driver.get('http://www.baidu.com')
 
 driver.find_element_by_id("kw").send_keys(u"中国selenium")
 time.sleep(5)
-print ("0000")
+print ("777")
+print("www.testertechnology.com")
 #通过id定位输入框，html规定，id在文档中必须唯一，类似于我们的身份证号
 #driver.find_element_by_id("su").click()
 #driver.quit()
